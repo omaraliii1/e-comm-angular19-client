fa68e8db666eea788f008116cc310a865bbf9e2e
omaraliii1 <omaraliyassin18@gmail.com>
2025-05-10 01:40:57 +0300

    generate the app component

---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { StorageService } from './_services/storage.service';
+import { AuthService } from './_services/auth.service';
+import { EventBusService } from './_shared/event-bus.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  private role: string = '';
+  isLoggedIn = false;
+  showAdminBoard = false;
+  username?: string;
+
+  eventBusSub?: Subscription;
+
+  constructor(
+    private storageService: StorageService,
+    private authService: AuthService,
+    private eventBusService: EventBusService
+  ) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.storageService.isLoggedIn();
+
+    if (this.isLoggedIn) {
+      const user = this.storageService.getUser();
+      this.role = user.role;
+
+      if (this.role === 'admin') {
+        this.showAdminBoard = true;
+      } else {
+        this.showAdminBoard = false;
+      }
+
+      this.username = user.username;
+    }
+
+    this.eventBusSub = this.eventBusService.on('logout', () => {
+      this.logout();
+    });
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
+}
